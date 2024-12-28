6a4b585e9cb90f78d91ea80fcbafe90ed2c527d9
hana feizi <hanafaizi83@gmail.com>
2024-12-28 09:39:19 +0330

    get characters and show them

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,27 @@ import './App.css'
 import CharacterList from './components/CharacterList'
 import Navbar from './components/Navbar'
 import ChatacterDetail from './components/ChatacterDetail'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { allCharacters } from '../data/data'
+import axios from 'axios'
+import toast, { Toaster } from 'react-hot-toast'
 
 function App() {
-  const [characters, setCharacters] = useState(allCharacters)
-
+  const [characters, setCharacters] = useState([])
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const { data } = await axios.get('https://rickandmortyapi.com/api/character');
+        setCharacters(data.results.slice(0,5))
+      } catch (error) {
+        toast.error(error.response.data.error)
+      }
+    }
+    fetchData()
+  }, [])
   return (
     <>
+      <Toaster />
       <Navbar />
       <div className="main">
         <CharacterList characters={characters} />
--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -1,34 +1,32 @@
 import { EyeIcon } from '@heroicons/react/24/outline'
 import React from 'react'
-function CharacterList({characters}) {
+function CharacterList({ characters }) {
     return (
         <div className="characters-list">
             {
-                characters.map(character => 
-                    < div className='list__item' key={character.id}>
-                        <img src={character.image} alt={character.name} />
-                        <h3 className='name'>
-                            <span>{character.gender == 'Male' ? '🧑' : '👩'}</span>
-                            <span>{character.name}</span>
-                        </h3>
-                        <div className='list-item__info info'>
-                            <span className={`status ${character.status === 'Dead' ? 'red' : ''}`}></span>
-                            <span> {character.status}</span>
-                            <span> - {character.species}</span>
-                        </div>
-                        <button className='icon red'>
-                            <EyeIcon />
-                        </button>
-                    </div >
-                )
+                characters.map(character => <Character character={character} />)
             }
 
         </div>
-
-
-
-
     )
 }
 
 export default CharacterList
+
+function Character({ character }) {
+    return < div className='list__item' key={character.id}>
+        <img src={character.image} alt={character.name} />
+        <h3 className='name'>
+            <span>{character.gender == 'Male' ? '🧑' : '👩'}</span>
+            <span>{character.name}</span>
+        </h3>
+        <div className='list-item__info info'>
+            <span className={`status ${character.status === 'Dead' ? 'red' : ''}`}></span>
+            <span> {character.status}</span>
+            <span> - {character.species}</span>
+        </div>
+        <button className='icon red'>
+            <EyeIcon />
+        </button>
+    </div >
+}
